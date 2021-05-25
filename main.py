80a7f26ac4d34e0d95a7759a89945e1b6c02e208
Shiv Kumar Singh <shivkumargwl@gmail.com>
2021-05-25 21:35:33 +0530

    dire structure setup # 3

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-@app.route("/index")
-@app.route("/home")
-def hello_world():
-    return "<h1>Hello, Flask!<h1>"
+from application import app
